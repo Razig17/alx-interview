acc5b791125cbb2d995a5d2eaffecce94a54166e
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-13 02:46:41 +0300

    Solve the N Queens puzzle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,53 +1,60 @@
 #!/usr/bin/python3
-"""Solve the n queens puzzle"""
+"""Solve the N queens puzzle."""
 import sys
-
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
-elif sys.argv[1].isdigit() is False:
+elif not sys.argv[1].isdigit():
     print("N must be a number")
     sys.exit(1)
 elif int(sys.argv[1]) < 4:
     print("N must be at least 4")
     sys.exit(1)
+
 N = int(sys.argv[1])
 
 
 def solve_n_queens(n):
-    """Solve the N queens problem for n x n chess board
+    """Solve the N queens problem for n x n chess board.
+
     Args:
-        n (int): The number of queens
+        n (int): The number of queens.
     """
     board = [[0] * n for _ in range(n)]
     pos_diag = set()
     neg_diag = set()
     cols = set()
 
-    def backtrack(r):
-        """Perform the backtracking on row r
+    def backtrack(row):
+        """Perform backtracking to find solutions.
+
+        Args:
+            row (int): The current row being explored.
         """
-        if (r == n):
-            res = []
-            [[res.append(c) for c in r if c] for r in board]
-            print(res)
+        if row == n:
+            result = []
+            [[result.append(c) for c in r if c] for r in board]
+            print(result)
             return
-        for c in range(n):
-            if (c in cols or (r - c) in pos_diag
-                    or (r + c) in neg_diag):
+
+        for col in range(n):
+            if col in cols or (row - col) in pos_diag\
+                    or (row + col) in neg_diag:
                 continue
 
-            board[r][c] = [r, c]
-            pos_diag.add(r - c)
-            neg_diag.add(r + c)
-            cols.add(c)
-            backtrack(r + 1)
-            pos_diag.remove(r - c)
-            neg_diag.remove(r + c)
-            cols.remove(c)
-            board[r][c] = 0
-        return
+            board[row][col] = [row, col]
+            pos_diag.add(row - col)
+            neg_diag.add(row + col)
+            cols.add(col)
+
+            backtrack(row + 1)
+
+            board[row][col] = 0
+            pos_diag.remove(row - col)
+            neg_diag.remove(row + col)
+            cols.remove(col)
+
     backtrack(0)
 
 
